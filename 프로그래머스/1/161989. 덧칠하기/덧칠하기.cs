101f30020f9e00deafba11e7bfe6d8ef9d907a26
WiGyuYeon <dnlrbdus31@gmail.com>
2025-04-08 21:38:31 +0900

    [level 1] Title: 덧칠하기, Time: 0.56 ms, Memory: 32.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class Solution {
+    public int solution(int n, int m, int[] section) {
+        int answer = 0;
+        int painted = 0;
+        for(int i=0;i<section.Length;i++){
+            if(section[i] <= painted) continue;
+            painted = section[i]+m-1;
+            answer++;
+        }
+        return answer;
+    }
+}
