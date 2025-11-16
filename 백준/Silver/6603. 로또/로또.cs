6cd04ca6441cdb384d778bcf5419827f426f684b
WiGyuYeon <dnlrbdus31@gmail.com>
2025-11-16 23:03:30 +0900

    [Silver II] Title: 로또, Time: 56 ms, Memory: 6280 KB -BaekjoonHub

--- 백준/Silver/6603. 로또/로또.cs
+++ 백준/Silver/6603. 로또/로또.cs
@@ -1,34 +1,42 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.WebSockets;
 using System.Numerics;
+using System.Reflection.Metadata.Ecma335;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading;
+using System.Transactions;
 
 namespace ConsoleApp1
 {
     class Program
     {
         static int k;
-        static int[] input;
         static List<int> result = new List<int>();
-
+        static int[] input;
+        static StringBuilder sb = new StringBuilder();
+        
         static void Main(string[] args)
         {
             while (true)
             {
                 input = Console.ReadLine().Split().Select(int.Parse).ToArray();
-                k = input[0];
-                
-                if (k == 0)
-                    break;
 
+                if (input[0] == 0) break;
+
+                k = input[0];
+
+                sb.Clear();
                 result.Clear();
 
                 DFS(0, 1);
+                Console.Write(sb.ToString());
                 Console.WriteLine();
             }
         }
@@ -37,17 +45,16 @@ namespace ConsoleApp1
         {
             if(depth == 6)
             {
-                Console.WriteLine(string.Join(" ", result));
+                sb.AppendLine(string.Join(" ", result));
                 return;
             }
 
-            for(int i = start; i <= k; i++)
+            for(int i=start; i <= k; i++)
             {
                 result.Add(input[i]);
                 DFS(depth + 1, i + 1);
                 result.RemoveAt(result.Count - 1);
             }
-
         }
     }
 }
