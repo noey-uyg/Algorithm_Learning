bef2960b79011ec83453ba77ecc501e1a73b74c6
WiGyuYeon <dnlrbdus31@gmail.com>
2025-12-10 13:19:46 +0900

    Time: 19 ms (33.18%), Space: 29.7 MB (21.44%) - LeetHub

--- LeetCode/Easy/1925-count-square-sum-triples/1925-count-square-sum-triples.cs
+++ LeetCode/Easy/1925-count-square-sum-triples/1925-count-square-sum-triples.cs
@@ -4,10 +4,11 @@ public class Solution {
 
         for(int i=1;i<=n;i++){
             for(int j=1;j<=n;j++){
-                int c = (int)Math.Sqrt(i*i + j*j);
+                int temp = i*i + j*j;
+                int c = (int)Math.Sqrt(temp);
 
                 if(c>n) break;
-                if(c<=n && c*c == i*i + j*j){
+                if(c<=n && c*c == temp){
                     result++;
                 }
             }
